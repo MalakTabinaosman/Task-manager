c64e8a12a4fdc091b164b63a0258279f40f94a59
MalakTabinaosman <malakosman182004@gmail.com>
2025-07-15 09:47:37 +0300

    Edit splash screen

--- lib/features/tasks/presentation/screens/home/splash_screen.dart
+++ lib/features/tasks/presentation/screens/home/splash_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:task_management_app/features/tasks/presentation/screens/onboarding_screens/onboarding_screen_1.dart';
-import 'home_screen.dart'; 
+import 'home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -14,32 +15,41 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _navigate();
-  }
 
-  Future<void> _navigate() async {
-    await Future.delayed(Duration(seconds: 2));
-    final prefs = await SharedPreferences.getInstance();
-    final seenOnboarding = prefs.getBool('seenOnboarding') ?? false;
+    Future.delayed(const Duration(seconds: 4), () async {
+      final prefs = await SharedPreferences.getInstance();
+      final seen = prefs.getBool('seenOnboarding') ?? false;
 
-    if (seenOnboarding) {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (_) => HomeScreen()),
+        MaterialPageRoute(
+          builder: (_) => seen ? HomeScreen() : OnboardingScreen1(),
+        ),
       );
-    } else {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => OnboardingScreen1()),
-      );
-    }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor, 
       body: Center(
-        child: Text("Task Manager", style: TextStyle(fontSize: 24)),
+        child: DefaultTextStyle(
+          style: const TextStyle(
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+          child: AnimatedTextKit(
+            animatedTexts: [
+              TypewriterAnimatedText(
+                'Tasky',
+                speed: const Duration(milliseconds: 150),
+              ),
+            ],
+            totalRepeatCount: 1,
+          ),
+        ),
       ),
     );
   }
