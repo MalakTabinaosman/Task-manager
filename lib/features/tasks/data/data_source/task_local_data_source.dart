f7028d2feeef471f257ac697629fb61305062e04
MalakTabinaosman <malakosman182004@gmail.com>
2025-08-27 12:39:22 +0300

    chore: initial commit (Flutter scaffold)

--- lib/features/tasks/data/data_source/task_local_data_source.dart
+++
@@ -1,6 +0,0 @@
-import 'package:task_management_app/features/tasks/data/models/task_model.dart';
-
-abstract class TaskLocalDataSource {
-  Future<List<TaskModel>> getTasks(); 
-  Future<void> saveTasks(List<TaskModel> tasks); 
-}
--- lib/features/tasks/domain/repository/task_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/task.dart';
-
-abstract class TaskRepository {
-  Future<void> addTask(Task task);
-  Future<void> deleteTask(String id);
-  Future<void> toggleTaskStatus(String id);
-  Future<List<Task>> getAllTasks();
-  Future<void> editTask(Task task);
-}
